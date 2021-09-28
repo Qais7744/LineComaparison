42ea76319659b0019ac844e96230457f956aedc4
unknown <altamashbagwan1@gmail.com>
2021-09-28 22:26:07 +0530

    Added: Functioning of Calculating of lenth

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,19 @@
 package javalinecomparison;
 
 public class LineComparison {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison");
+		// Function to calculate distance
+	static double distance (int x1, int y1, int x2, int y2)
+	{
+		// Calculating distance
+		double distance;
+		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		return distance;
 	}
-
+		public static void main(String[] args)
+		{
+			LineComparison linecomparison = new LineComparison();
+			double value = linecomparison.distance(4, 5, 6, 7);
+			System.out.println("calculate distance between two points : " +value);
+		}
+	
 }
